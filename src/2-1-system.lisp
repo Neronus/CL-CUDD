b175b1bcf67c47abee520db138fc25221d5b5b62
Masataro Asai <guicho2.71828@gmail.com>
2016-07-20 14:13:00 +0900

    made functions in 2-1-system more plain

--- src/2-1-system.lisp
+++ src/2-1-system.lisp
@@ -1,23 +1,25 @@
 (in-package :cudd)
 
-(def-cudd-call disable-gc ((:common cudd-disable-garbage-collection))
-               :generic "Disables garbage collection. Garbage
+(defun disable-gc ()
+  "Disables garbage collection. Garbage
 collection is initially enabled. This function may be called to
 disable it. However, garbage collection will still occur when a new
 node must be created and no memory is left, or when garbage collection
 is required for correctness. (E.g., before reordering.)"
-               :dont-wrap-result t)
+  (cudd-disable-garbage-collection
+   (manager-pointer *manager*)))
 
-(def-cudd-call enable-gc ((:common cudd-enable-garbage-collection))
-  :generic "Enables garbage collection. Garbage collection is
+(defun enable-gc ()
+  "Enables garbage collection. Garbage collection is
 initially enabled. Therefore it is necessary to call this function
 only if garbage collection has been explicitly disabled."
-  :dont-wrap-result t)
+  (cudd-enable-garbage-collection
+   (manager-pointer *manager*)))
 
-(def-cudd-call set-background ((:common cudd-set-background) bck)
-  :generic "Sets the background constant of the manager. It assumes
+(defun set-background (bck)
+  "Sets the background constant of the manager. It assumes
 that the DdNode pointer bck is already referenced."
-  :dont-wrap-result t)
+  (cudd-set-background (manager-pointer *manager*) bck))
 
 (defun count-leaves (node)
   "Counts the number of leaves in a DD."
@@ -33,3 +35,18 @@ that the DdNode pointer bck is already referenced."
 (defmethod cffi:translate-to-foreign (pointer (manager manager))
   (manager-pointer manager))
 
+;; (def-cudd-call disable-reordering ((:common cudd-disable-garbage-collection))
+;;                :generic "Disables garbage collection. Garbage
+;; collection is initially enabled. This function may be called to
+;; disable it. However, garbage collection will still occur when a new
+;; node must be created and no memory is left, or when garbage collection
+;; is required for correctness. (E.g., before reordering.)"
+;;                :dont-wrap-result t)
+;; 
+;; (def-cudd-call enable-reordering ((:common cudd-enable-garbage-collection))
+;;   :generic "Enables garbage collection. Garbage collection is
+;; initially enabled. Therefore it is necessary to call this function
+;; only if garbage collection has been explicitly disabled."
+;;   :dont-wrap-result t)
+
+;; cudd-auto-dyn-enable
