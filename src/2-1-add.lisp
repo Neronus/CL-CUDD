07722138c049fe55e17ea9b07f6c82930a078650
Masataro Asai <guicho2.71828@gmail.com>
2017-03-11 11:13:48 +0900

    [enhancement] add-apply shows valid operations only

--- src/2-1-add.lisp
+++ src/2-1-add.lisp
@@ -2,38 +2,6 @@
 
 (in-package :cudd)
 
-(def-cudd-call add-apply ((:add cudd-add-apply) op (f :node) (g :node))
-  :generic "Applies op to the corresponding discriminants of f and g.
-
-The following operations are supported:
-
-+plus+ - Integer and floating point addition
-+times+ - Integer and floating point multiplication.
-+threshold+ - Threshold operator for Apply (f if f >=g; 0 if f<g)
-+set-NZ+ - This operator sets f to the value of g wherever g != 0.
-+divide+ - Integer and floating point division.
-+minus+ - Integer and floating point substraction.
-+minimum+ - Integer and floating point minimum.
-+maximum+ - Integer and floating point maximum.
-+one-zero-maximum+ - 1 if f > g and 0 otherwise.
-+diff+ - Returns NULL if not a terminal case; f op g otherwise, where f op g is plusinfinity if f=g; min(f,g) if f!=g.
-+agreement+ - op g,  where f op g is f if f==g; background if f!=g.
-+or+ - Disjunction of two 0-1 ADDs.
-+nand+ - NAND of two 0-1 ADDs.
-+nor+ - NOR of two 0-1 ADDs.
-+xor+ - XOR of two 0-1 ADDs.
-+xnor+ - XNOR of two 0-1 ADDs.
-+equals+ - f op g, where f op g is 1 if f==g, 0 otherwise
-+not-equals+ - f op g, where f op g is 1 if f!=g; 0 otherwise
-+greater-than+ - f > g, where f op g is 1 if f!=g; 0 otherwise
-+greater-than-equals+ -  if f >= g, 0 otherwise
-+less-than+ -  < g, where f op g is 1 if f!=g; 0 otherwise
-+less-than-equals+ -  <= g, where f op g is 1 if f!=g; 0 otherwise
-+pow+ - f to the power of g
-+mod+ - f modulo g
-+log-x-y+ - log f base g"
-  op)
-
 (def-cudd-call add-negate ((:add cudd-add-negate) (node :node))
   :generic "Computes the additive inverse of an ADD.")
 
@@ -48,57 +16,75 @@ The following operations are supported:
   :generic
   "Return node with value -infinity.")
 
-
 ;;; Functions for add-apply
-(defparameter +plus+ (cffi:foreign-symbol-pointer "Cudd_addPlus")
-  "Integer and floating point addition")
-(defparameter +times+ (cffi:foreign-symbol-pointer "Cudd_addTimes")
-  "Integer and floating point multiplication.")
-(defparameter +threshold+ (cffi:foreign-symbol-pointer "Cudd_addThreshold")
-  "Threshold operator for Apply (f if f >=g; 0 if f<g)")
-(defparameter +set-NZ+ (cffi:foreign-symbol-pointer "Cudd_addSetNZ")
-  "This operator sets f to the value of g wherever g != 0.")
-(defparameter +divide+ (cffi:foreign-symbol-pointer "Cudd_addDivide")
-  "Integer and floating point division.")
-(defparameter +minus+ (cffi:foreign-symbol-pointer "Cudd_addMinus")
-  "Integer and floating point substraction.")
-(defparameter +minimum+ (cffi:foreign-symbol-pointer "Cudd_addMinimum")
-  "Integer and floating point minimum.")
-(defparameter +maximum+ (cffi:foreign-symbol-pointer "Cudd_addMaximum")
-  "Integer and floating point maximum.")
-(defparameter +one-zero-maximum+ (cffi:foreign-symbol-pointer "Cudd_addOneZeroMaximum")
-  "1 if f > g and 0 otherwise.")
-(defparameter +diff+ (cffi:foreign-symbol-pointer "Cudd_addDiff")
-  "f op g , where f op g is plusinfinity if f=g; min(f,g) if f!=g.")
-(defparameter +agreement+ (cffi:foreign-symbol-pointer "Cudd_addAgreement")
-  "f op g,  where f op g is f if f==g; background if f!=g.")
-(defparameter +or+ (cffi:foreign-symbol-pointer "Cudd_addOr")
-  "Disjunction of two 0-1 ADDs.")
-(defparameter +and+ (cffi:foreign-symbol-pointer "Cudd_addAnd")
-  "Conjunction of two 0-1 ADDs.")
-(defparameter +nand+ (cffi:foreign-symbol-pointer "Cudd_addNand")
-  "NAND of two 0-1 ADDs.")
-(defparameter +nor+ (cffi:foreign-symbol-pointer "Cudd_addNor")
-  "NOR of two 0-1 ADDs.")
-(defparameter +xor+ (cffi:foreign-symbol-pointer "Cudd_addXor")
-  "XOR of two 0-1 ADDs.")
-(defparameter +xnor+ (cffi:foreign-symbol-pointer "Cudd_addXnor")
-  "XNOR of two 0-1 ADDs.")
-(defparameter +equals+ (cffi:foreign-symbol-pointer "Cudd_addEquals")
-  "f op g, where f op g is 1 if f==g, 0 otherwise")
-(defparameter +not-equals+ (cffi:foreign-symbol-pointer "Cudd_addNotEquals")
-  "f op g, where f op g is 1 if f!=g; 0 otherwise")
-(defparameter +greater-than+ (cffi:foreign-symbol-pointer "Cudd_addGreaterThan")
-  "f > g, where f op g is 1 if f!=g; 0 otherwise")
-(defparameter +greater-than-equals+ (cffi:foreign-symbol-pointer "Cudd_addGreaterThanEquals")
-  "1 if f >= g, 0 otherwise")
-(defparameter +less-than+ (cffi:foreign-symbol-pointer "Cudd_addLessThan")
-  "f < g, where f op g is 1 if f!=g; 0 otherwise")
-(defparameter +less-than-equals+ (cffi:foreign-symbol-pointer "Cudd_addLessThanEquals")
-  "f <= g, where f op g is 1 if f!=g; 0 otherwise")
-(defparameter +pow+ (cffi:foreign-symbol-pointer "Cudd_addPow")
-  "f to the power of g")
-(defparameter +mod+ (cffi:foreign-symbol-pointer "Cudd_addMod")
-  "f modulo g")
-(defparameter +log-x-y+ (cffi:foreign-symbol-pointer "Cudd_addLogXY")
-  "log f base g")
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defvar *add-operators* nil)
+  (defun probe-symbol (name sym)
+    (let ((fsym (cffi:foreign-symbol-pointer name)))
+      (if fsym
+          (pushnew (cons sym name) *add-operators* :test #'equalp)
+          (format *error-output* "~&C Symbol ~a for ~a was not found~&" name sym))
+      fsym))
+
+  ;; some of these symbols are obsoleted; see *add-operators* and the docstring of add-apply
+  (defparameter +plus+ (probe-symbol "Cudd_addPlus" '+plus+)
+    "Integer and floating point addition")
+  (defparameter +times+ (probe-symbol "Cudd_addTimes" '+times+)
+    "Integer and floating point multiplication.")
+  (defparameter +threshold+ (probe-symbol "Cudd_addThreshold" '+threshold+)
+    "Threshold operator for Apply (f if f >=g; 0 if f<g)")
+  (defparameter +set-NZ+ (probe-symbol "Cudd_addSetNZ" '+set-NZ+)
+    "This operator sets f to the value of g wherever g != 0.")
+  (defparameter +divide+ (probe-symbol "Cudd_addDivide" '+divide+)
+    "Integer and floating point division.")
+  (defparameter +minus+ (probe-symbol "Cudd_addMinus" '+minus+)
+    "Integer and floating point substraction.")
+  (defparameter +minimum+ (probe-symbol "Cudd_addMinimum" '+minimum+)
+    "Integer and floating point minimum.")
+  (defparameter +maximum+ (probe-symbol "Cudd_addMaximum" '+maximum+)
+    "Integer and floating point maximum.")
+  (defparameter +one-zero-maximum+ (probe-symbol "Cudd_addOneZeroMaximum" '+one-zero-maximum+)
+    "1 if f > g and 0 otherwise.")
+  (defparameter +diff+ (probe-symbol "Cudd_addDiff" '+diff+)
+    "f op g , where f op g is plusinfinity if f=g; min(f,g) if f!=g.")
+  (defparameter +agreement+ (probe-symbol "Cudd_addAgreement" '+agreement+)
+    "f op g,  where f op g is f if f==g; background if f!=g.")
+  (defparameter +or+ (probe-symbol "Cudd_addOr" '+or+)
+    "Disjunction of two 0-1 ADDs.")
+  (defparameter +and+ (probe-symbol "Cudd_addAnd" '+and+)
+    "Conjunction of two 0-1 ADDs.")
+  (defparameter +nand+ (probe-symbol "Cudd_addNand" '+nand+)
+    "NAND of two 0-1 ADDs.")
+  (defparameter +nor+ (probe-symbol "Cudd_addNor" '+nor+)
+    "NOR of two 0-1 ADDs.")
+  (defparameter +xor+ (probe-symbol "Cudd_addXor" '+xor+)
+    "XOR of two 0-1 ADDs.")
+  (defparameter +xnor+ (probe-symbol "Cudd_addXnor" '+xnor+)
+    "XNOR of two 0-1 ADDs.")
+  (defparameter +equals+ (probe-symbol "Cudd_addEquals" '+equals+)
+    "f op g, where f op g is 1 if f==g, 0 otherwise")
+  (defparameter +not-equals+ (probe-symbol "Cudd_addNotEquals" '+not-equals+)
+    "f op g, where f op g is 1 if f!=g; 0 otherwise")
+  (defparameter +greater-than+ (probe-symbol "Cudd_addGreaterThan" '+greater-than+)
+    "f > g, where f op g is 1 if f!=g; 0 otherwise")
+  (defparameter +greater-than-equals+ (probe-symbol "Cudd_addGreaterThanEquals" '+greater-than-equals+)
+    "1 if f >= g, 0 otherwise")
+  (defparameter +less-than+ (probe-symbol "Cudd_addLessThan" '+less-than+)
+    "f < g, where f op g is 1 if f!=g; 0 otherwise")
+  (defparameter +less-than-equals+ (probe-symbol "Cudd_addLessThanEquals" '+less-than-equals+)
+    "f <= g, where f op g is 1 if f!=g; 0 otherwise")
+  (defparameter +pow+ (probe-symbol "Cudd_addPow" '+pow+)
+    "f to the power of g")
+  (defparameter +mod+ (probe-symbol "Cudd_addMod" '+mod+)
+    "f modulo g")
+  (defparameter +log-x-y+ (probe-symbol "Cudd_addLogXY" '+log-x-y+)
+    "log f base g"))
+
+(def-cudd-call add-apply ((:add cudd-add-apply) op (f :node) (g :node))
+  :generic #.(format nil "Applies op to the corresponding discriminants of f and g.
+
+The following operations are supported:
+
+~{~A~%~}" (mapcar (lambda (op)
+                 (format nil "~a (originally ~a) -- ~a" (car op) (cdr op) (documentation (car op) 'variable)))
+               *add-operators*)))
